87a3db19dd35a22645cd0b45d64a449f1ce74ca8
ar1oh <ignite792000@gmail.com>
2022-07-18 02:48:54 +0400

    UI

---
+++ Assets/Game/Scripts/HungerBar_Script.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HungerBar_Script : MonoBehaviour
+{
+    private Image speedBar;
+    public Image color1;
+    public Image color2;
+    public Image color3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedBar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        color1.fillAmount = speedBar.fillAmount*3;
+        if (color1.fillAmount == 1) color2.fillAmount = speedBar.fillAmount*3 - color1.fillAmount;
+        else color2.fillAmount = 0;
+        if (color2.fillAmount == 1) color3.fillAmount = speedBar.fillAmount*3 - color2.fillAmount - color1.fillAmount;
+        else color3.fillAmount = 0;
+    }
+}
