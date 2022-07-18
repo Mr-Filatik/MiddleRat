e6e8ea4c4e11bcffe855a05a8d6d16baaf5f585c
ar1oh <ignite792000@gmail.com>
2022-07-19 02:51:51 +0400

    UI update

---
+++ Assets/Game/Scripts/MainMenu_Script.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Script : MonoBehaviour
+{
+    public void playGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
